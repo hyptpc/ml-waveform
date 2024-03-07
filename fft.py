d478589dd57f79bc0d72af71413328dd6eecc197
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2024-03-07 17:42:05 +0900

    Update fft.py using root file directly.

--- fft.py
+++ fft.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 
 '''
-Load wav file and do fft.
+Load wav/root file and do fft.
 '''
 
 import argparse
 import logging
 import logging.config
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import os
 import yaml
 
 import torch
 import torchaudio
+import uproot
 
 top_dir = os.path.dirname(os.path.abspath(__file__))
 logger = logging.getLogger(__name__)
 
 #______________________________________________________________________________
-def run(file_path):
-  ''' run '''
-  waveform, sample_rate = torchaudio.load(file_path)
-  logger.info(f'Shape of waveform [channel, time]: {waveform.size()}')
-  logger.info(f'Sample rate of waveform: {sample_rate}')
-  waveform = waveform[0] # first channel
-  waveform = (waveform - waveform.mean()) #/waveform.std() # normalize
+def fft(waveform, sample_rate, show=True, outfig='fft.png'):
+  ''' fft '''
   fft_result = torch.fft.fft(waveform)
   freq_axis = torch.fft.fftfreq(waveform.size(0), d=1./sample_rate)
   logger.debug(f'fft result: {fft_result}')
@@ -33,7 +30,7 @@ def run(file_path):
   plt.figure(figsize=(12, 6))
   plt.subplot(2, 1, 1)
   plt.plot(waveform.t().numpy())
-  plt.title(f'waveform : {os.path.basename(file_path)}')
+  plt.title(f'waveform')
   plt.xlabel('Sample')
   plt.ylabel('Amplitude')
   plt.subplot(2, 1, 2)
@@ -43,14 +40,66 @@ def run(file_path):
   plt.xlabel('Frequency (Hz)')
   plt.ylabel('Amplitude')
   plt.tight_layout()
-  plt.savefig('fft.png')
-  plt.show()
+  if show:
+    plt.savefig(outfig)
+    plt.show()
+  else:
+    outfig.savefig()
+  plt.close()
+
+#______________________________________________________________________________
+def run(file_path):
+  ''' run '''
+  _, input_ext = os.path.splitext(parsed.file_path)
+  if input_ext == '.wav':
+    run_wav(parsed.file_path)
+  elif input_ext == '.root':
+    run_root(parsed.file_path)
+  else:
+    logger.error(f'unknown file type: {file_path}')
+
+#______________________________________________________________________________
+def run_root(file_path, sample_rate=12500000):
+  ''' run using root file '''
+  try:
+    logger.info(f'open {file_path}')
+    tree = uproot.concatenate(file_path+':tpc',
+                              filter_name=['runnum', 'evnum',
+                                           'rpadTpc', 'rwavTpc'],
+                              library='ak')
+  except uproot.KeyInFileError as e:
+    logger.error(e)
+    return
+  run_number = tree['runnum'][0]
+  evnum = tree['evnum']
+  rpadTpc = tree['rpadTpc']
+  rwavTpc = tree['rwavTpc']
+  pdf = PdfPages('fft.pdf')
+  for i in range(len(tree)):
+    logger.info(f'run{run_number:05d} ev{evnum[i]:06d} {len(rpadTpc[i])} {len(rwavTpc[i])}')
+    for j in range(len(rpadTpc[i])):
+      logger.info(f'{rpadTpc[i][j]}, {rwavTpc[i][j]}')
+      waveform = torch.Tensor(rwavTpc[i][j]) / (2**12)
+      waveform = (waveform - waveform.mean())
+      fft(waveform, sample_rate, show=False, outfig=pdf)
+    break
+  pdf.close()
+
+#______________________________________________________________________________
+def run_wav(file_path):
+  ''' run using wav file '''
+  waveform, sample_rate = torchaudio.load(file_path)
+  logger.info(f'Shape of waveform [channel, time]: {waveform.size()}')
+  logger.info(f'Sample rate of waveform: {sample_rate}')
+  waveform = waveform[0] # first channel
+  waveform = (waveform - waveform.mean())
+  fft(waveform, sample_rate)
 
 #______________________________________________________________________________
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('file_path',
-                      help=('file path of input wav'))
+                      help=('file path of input file (.wav or .root)'))
   parsed, unpased = parser.parse_known_args()
   log_conf = os.path.join(top_dir, 'logging_config.yml')
   with open(log_conf, 'r') as f:
